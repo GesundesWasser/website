37aee594e274d76e77984c0ce29fb55addcf3d0d
GesundesWasser <git@wagger.dev>
2024-07-07 02:05:17 +0200

    Dynamically get the hostname

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 "use strict";
 
 $(window).on('load', function() {
-    $('#header-text').text("mcdonelts.city");
+    $('#header-text').text(window.location.hostname);
     $('.logo').attr('src', 'https://download.scamcraft.net/img/wwagoinc.png');
 });
