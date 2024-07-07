8d7d1a912a8d006a24fde286d7fd4b9d7ef09784
GesundesWasser <git@wagger.dev>
2024-07-07 02:06:02 +0200

    Redo the Footer

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,19 @@
 "use strict";
 
-$(window).on('load', function() {
-    const currentYear = new Date().getFullYear();
-    $('#copyright-year').text(currentYear);
-    $('#site-name-version').text("Codename Kapselordnung Release v1.9 -> R3");
-});
+// An IIFE to avoid polluting global scope
+(function() {
+    $(document).ready(function() {
+        // Update the copyright year
+        $('#copyright-year').text("2024 -> " + new Date().getFullYear());
+
+        // Set the Codename, version, revision, testbuild and Copyright Year.
+        const CODENAME = "Kapselordnung";
+        const VERSION = "v1.10";
+        const REVISION = "0";
+        const TEST_BUILD = true;
+        const TEST_BUILD_MSG = TEST_BUILD ? "This version of Codename " + CODENAME + " also looks Experimental!" : "";
+        $('#site-name-version').text("Codename " + CODENAME + " " + VERSION + " -> R" + REVISION);
+        $('#testbuild').text(TEST_BUILD_MSG);
+        $('#copyright-year').text("2024 -> " + new Date().getFullYear());
+    });
+})();
