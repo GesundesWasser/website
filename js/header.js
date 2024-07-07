2b5771e27b6b54db8a31454981df27bb8e3bbb74
GesundesWasser <git@wagger.dev>
2024-07-07 19:32:04 +0200

    Move the OnLoad to OnReady

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 "use strict";
 
-$(window).on('load', function() {
+$(document).ready(function() {
     $('#header-text').text(window.location.hostname);
     $('.logo').attr('src', 'https://download.scamcraft.net/img/wwagoinc.png');
+    $('#logo-link').attr('href', 'javascript:');
 });
