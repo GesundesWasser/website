9bc783cfbfd53145d0bf90aebdcf9f969b5d2b77
GesundesWasser <git@wagger.dev>
2026-02-25 22:30:10 +0100

    fuck light theme

--- js/theme.js
+++ js/theme.js
@@ -1,51 +1,9 @@
 "use strict";
 import $ from 'jquery';
-// Function to initialize the theme
+// Always use dark theme
 function loadTheme() {
     $(document).ready(function() {
-        const $darkButton = $('#dark');
-        const $lightButton = $('#light');
-        const $solarButton = $('#solar');
-        const $body = $('body');
-
-        const theme = localStorage.getItem('theme');
-        const isSolar = localStorage.getItem('isSolar');
-
-        if (theme) {
-            $body.addClass(theme);
-            if (isSolar) {
-                $body.addClass('solar');
-                $solarButton.text("normalize");
-                $solarButton.css('--bg-solar', 'white');
-            }
-        } else {
-            $body.addClass('dark');
-            localStorage.setItem('theme', 'dark');
-        }
-
-        $darkButton.on('click', () => {
-            $body.removeClass('light').addClass('dark');
-            localStorage.setItem('theme', 'dark');
-        });
-
-        $lightButton.on('click', () => {
-            $body.removeClass('dark').addClass('light');
-            localStorage.setItem('theme', 'light');
-        });
-
-        $solarButton.on('click', () => {
-            if ($body.hasClass('solar')) {
-                $body.removeClass('solar');
-                $solarButton.css('--bg-solar', 'var(--yellow)');
-                $solarButton.text('Solarize');
-                localStorage.removeItem('isSolar');
-            } else {
-                $solarButton.css('--bg-solar', 'white');
-                $body.addClass('solar');
-                $solarButton.text('Normalize');
-                localStorage.setItem('isSolar', true);
-            }
-        });
+        $('body').addClass('dark');
     });
 }
 
